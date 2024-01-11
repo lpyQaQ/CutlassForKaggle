d47b90de6a80258739559788a52ce898ed582051
Puyuan <102471525+lpyQaQ@users.noreply.github.com>
2024-01-11 10:57:14 +0800

    Update backward_filter_fp16.cu

--- examples/19_large_depthwise_conv2d_torch_extension/backward_filter_fp16.cu
+++ examples/19_large_depthwise_conv2d_torch_extension/backward_filter_fp16.cu
@@ -188,8 +188,10 @@ torch::Tensor backward_filter_fp16(torch::Tensor diff, torch::Tensor input,
 
     Options options = Options();
 
-    options.update({input.size(0), input.size(2), input.size(3), input.size(1)},
-                   {weight.size(0), weight.size(2), weight.size(3), 1});
+    options.update(
+        {(int)input.size(0), (int)input.size(2), (int)input.size(3), (int)input.size(1)},
+        {(int)weight.size(0), (int)weight.size(2), (int)weight.size(3), 1});
+
 
     cutlass::TensorRef<ElementSrc, LayoutSrc> d_src(
             (ElementSrc*)input.data_ptr(),
